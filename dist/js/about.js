849038506bd929eb6905d080d4d2972d978750fa
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-10-22 18:15:22 +0100

    added janky mouseover events for names

--- dist/js/about.js
+++ dist/js/about.js
@@ -1,3 +1,33 @@
-$(document).on('load',function {
-    
+var singyImg = document.getElementById("singyImg");
+var singyName = document.getElementById("singyH1");
+var screamyImg = document.getElementById("screamyImg");
+var screamyName = document.getElementById("screamyH1");
+var rappyImg = document.getElementById("rappyImg");
+var rappyName = document.getElementById("rappyH1");
+singyName.hidden = true;
+screamyName.hidden = true;
+rappyName.hidden = true;
+
+singyImg.addEventListener("mouseover", function (event) {
+  singyName.hidden = false;
+});
+
+singyImg.addEventListener("mouseout", function () {
+  singyName.hidden = true;
+});
+
+screamyImg.addEventListener("mouseover", function () {
+  screamyName.hidden = false;
+});
+
+screamyImg.addEventListener("mouseout", function () {
+  screamyName.hidden = true;
+});
+
+rappyImg.addEventListener("mouseover", function () {
+  rappyName.hidden = false;
+});
+
+rappyImg.addEventListener("mouseout", function () {
+  rappyName.hidden = true;
 });
