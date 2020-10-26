a920e9e397ab76718993a3f89ee8be6c8fa11430
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-10-26 12:46:25 +0000

    cleanup

--- dist/js/global.js
+++ dist/js/global.js
@@ -52,29 +52,6 @@ $(document).ready(function () {
   }
 
   countdownTimer();
-  /*
-  // Update the count down every 1 second
-  var generateTimer = setInterval(function () {
-    // Get today's date and time
-    var now = new Date().getTime();
-
-    // Find the difference between now and the count down date
-    var difference = releaseDateTime - now;
-    // alert(difference);
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    var hours = Math.floor(
-      (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((difference % (1000 * 60)) / 1000);
-
-    // Display the result in the element with id="demo"
-    document.getElementById("newAlbumReleaseTimer").innerHTML =
-      days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-
-    // If the count down is finished, write some text
-  }, 1000);*/
   /////////////////////////////////////////////////////////////////////////////////////////////
   /////////////////////////////////////////////////////////////////////////////////////////////
   /////////////////////////////////////////////////////////////////////////////////////////////
