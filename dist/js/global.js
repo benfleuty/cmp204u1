d0efb8e08ba02b88bed86d2ff5b1ee95a7c0f209
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-10-26 12:31:22 +0000

    altered countdown timer

--- dist/js/global.js
+++ dist/js/global.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
   $.cookit({
     linkUrl: "https://www.jqueryscript.net/privacy/",
   });
+
   //
   /////////////////////////////////////////////////////////////////////////////////////////////
   // release timer
@@ -10,15 +11,47 @@ $(document).ready(function () {
   var albumName = "new album name";
   document.getElementById("newAlbumName").innerHTML =
     "<strong><em>" + albumName + "</em></strong>";
-  var releaseDate = new Date("Jan 1, 2021 00:00:00").getTime();
+  var releaseDateTimeTime = new Date("Jan 1, 2021 00:00:00").getTime();
 
+  function countdownTimer() {
+    setInterval(function () {
+      // gets the current date and time
+      var currentDateTime = new Date().getTime();
+      // calculates and stores the time between now and the release date
+      var deltaT = releaseDateTimeTime - currentDateTime;
+      /* Calculate individual units of time START */
+      var seconds = (deltaT % (1000 * 60)) / 1000;
+      var minutes = (deltaT % (1000 * 60 * 60)) / (1000 * 60);
+      var hours = (deltaT % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60);
+      var days = deltaT % (1000 * 60 * 60 * 24);
+      /* Calculate individual units of time END */
+      // Build the resultant string
+
+      var output =
+        days +
+        "days, " +
+        hours +
+        "hours, " +
+        minutes +
+        "minutes, " +
+        seconds +
+        "seconds ";
+
+      document.getElementById("newAlbumReleaseTimer").innerHTML = output;
+
+      //
+    }, 1000);
+  }
+
+  countdownTimer();
+  /*
   // Update the count down every 1 second
   var generateTimer = setInterval(function () {
     // Get today's date and time
     var now = new Date().getTime();
 
     // Find the difference between now and the count down date
-    var difference = releaseDate - now;
+    var difference = releaseDateTime - now;
     // alert(difference);
     // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(difference / (1000 * 60 * 60 * 24));
@@ -38,7 +71,7 @@ $(document).ready(function () {
       document.getElementById("newAlbumReleaseTimer").innerHTML =
         "NEW ALBUM RELEASED";
     }
-  }, 1000);
+  }, 1000);*/
   /////////////////////////////////////////////////////////////////////////////////////////////
   /////////////////////////////////////////////////////////////////////////////////////////////
   /////////////////////////////////////////////////////////////////////////////////////////////
